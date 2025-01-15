e5640a5c8600cd833dcfd91a4e1c0e7a2dbb5bf0
tasfique86 <tasfiquerishad1217@gmai.com>
2025-01-16 00:30:59 +0600

    first commit

---
+++ src/main/java/com/scm/scm20/controller/PageController.java
@@ -0,0 +1,46 @@
+package com.scm.scm20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String Home(Model model) {
+
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String About(Model model) {
+
+        return "about";
+    }
+
+
+    @GetMapping("/services")
+    public String Service() {
+
+        return "services";
+    }
+
+    @GetMapping("/register")
+    public String Register() {
+
+        return "register";
+    }
+
+    @GetMapping("/contact")
+    public String Contact() {
+
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String Login() {
+
+        return "login";
+    }
+}
