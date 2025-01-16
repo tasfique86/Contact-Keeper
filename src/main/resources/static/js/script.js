8ded89e5532ba6513c35cfd573463c8072cf9c6d
tasfique86 <tasfiquerishad1217@gmai.com>
2025-01-16 19:44:17 +0600

    change theme work

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,52 @@
+console.log("Script loaded!");
+
+let currentTheme=getTheme();
+
+changeTheme();
+
+
+function changeTheme(){
+
+
+    changePageTheme(currentTheme,currentTheme);
+
+    const changeThemeBtn = document.querySelector("#them_change_button");
+
+
+    changeThemeBtn.addEventListener("click", () => {
+
+        const oldTheme = currentTheme;
+        console.log("changeTheme");
+
+        if(currentTheme === "dark"){
+            currentTheme="light";
+        }
+        else {
+            currentTheme = "dark";
+        }
+
+        changePageTheme(currentTheme,oldTheme);
+    });
+}
+
+
+function setTheme(theme){
+    localStorage.setItem("theme", theme);
+}
+
+function getTheme(){
+    let theme = localStorage.getItem("theme");
+    if(theme) return   theme;
+    else  return "light";
+}
+
+
+
+function changePageTheme(theme,oldTheme){
+    setTheme(theme);
+
+    document.querySelector("html").classList.remove(oldTheme);
+    document.querySelector("html").classList.add(theme);
+    document.querySelector("#them_change_button").querySelector("span").textContent = currentTheme === "light" ? "Dark" : "Light";
+
+}
