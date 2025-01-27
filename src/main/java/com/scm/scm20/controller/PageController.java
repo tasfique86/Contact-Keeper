5a92f9e7dce42fd4fe94f6a3e01aedf4429036d5
tasfique86 <tasfiquerishad1217@gmail.com>
2025-01-27 14:14:34 +0600

    git check

--- src/main/java/com/scm/scm20/controller/PageController.java
+++ src/main/java/com/scm/scm20/controller/PageController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+//check
+//check 2
 @Controller
 public class PageController {
 
