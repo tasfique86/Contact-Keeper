69b4d4d98a8dd268e7361e5a27dd0c25212223f7
tasfique86 <tasfiquerishad1217@gmail.com>
2025-02-02 01:43:29 +0600

    Design add Contact page

---
+++ src/main/java/com/scm/scm20/controller/ContactController.java
@@ -0,0 +1,17 @@
+package com.scm.scm20.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/contacts")
+public class ContactController {
+
+    @RequestMapping("/add")
+    public String addContact(){
+        return "user/add_contact";
+
+    }
+
+}
--- src/main/java/com/scm/scm20/entities/Contact.java
+++ src/main/java/com/scm/scm20/entities/Contact.java
@@ -1,11 +1,17 @@
 package com.scm.scm20.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Contact {
 
     @Id
